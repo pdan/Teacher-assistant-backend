377c16401680eb9d942d4188a5d980f5f1a08337
Pourya Daneshvar <pourya@daneshvar.me>
2020-12-28 17:36:27 +0330

    First commit

---
+++ src/app/entities/todo.entity.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Todo extends BaseEntity {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  text: string
+
+  @Column()
+  date: number
+
+}
---
+++ src/migrations/1608324165096-migration.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class migration1608324165096 implements MigrationInterface {
+    name = 'migration1608324165096'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`CREATE TABLE "todo" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL, "text" varchar NOT NULL, "date" integer NOT NULL)`, undefined);
+        await queryRunner.query(`CREATE TABLE "user" ("id" integer PRIMARY KEY AUTOINCREMENT NOT NULL)`, undefined);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE "user"`, undefined);
+        await queryRunner.query(`DROP TABLE "todo"`, undefined);
+    }
+
+}
